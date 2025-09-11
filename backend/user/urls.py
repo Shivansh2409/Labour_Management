1a2805d6f52b289766ce783a1c4d806f03ceccdf
shivani <shivanisonibst2006@gamil.com>
2025-09-11 16:03:51 +0530

    urls

---
+++ backend/user/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from .views import ProfileDetailView
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('main/', views.main_view, name='main'),
+    path('profile/', ProfileDetailView.as_view(), name='user-profile'),
+]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics, permissions
+from .models import Profile
+from .serializers import ProfileSerializer
+
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
+
+
+
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signup_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    email = request.data.get('email','')
+    if not username or not password:
+        return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+    user = User.objects.create_user(username=username, password=password, email=email)
+    #log the user in (create session)
+    login(request, user)
+    return Response({'message': 'User created and logged in', 'username': user.username})
+
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    if not username or not password:
+        return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(request, username=username, password=password)
+    if user is None:
+        return Response({'error': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
+    login(request, user)
+    return Response({'message': 'Logged in', 'username': user.username})
+
+@api_view(['POST'])
+def logout_view(request):
+    logout(request)
+    return Response({'message': 'logged out'})
+
+@api_view(['GET'])
+def main_view(request):
+    if not request.user or not request.user.is_authenticated:
+        return Response({'error': 'not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+    return Response({'message':f'Welcome to the main page, {request.user.username}!'})
